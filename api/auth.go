dfa2f80b4c7893170c1c5c77f1548ffb313b23b5
Rafael Alkmim <rafael.alkmim@losch.lu>
2023-05-29 23:06:55 +0100

    server fixed

--- api/auth.go
+++ api/auth.go
@@ -15,8 +15,7 @@ func StartAuth(r *gin.RouterGroup) {
 	authHandler := NewAuthHandler(auth.NewService(auth.NewRepository()))
 
 	r.Use(cors.New(cors.Config{
-		AllowAllOrigins: 	true,
-		// AllowOrigins: 		[]string{"http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:5500"},
+		AllowOrigins: 		[]string{"http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:5500", "https://rarefinds.herokuapp.com"},
 		AllowMethods: 		[]string{"PUT","PATCH","GET","DELETE","POST","OPTIONS"},
 		AllowHeaders: 		[]string{"Origin","Content-type","Authorization","Content-Length","Content-Language",
 										"Content-Disposition","User-Agent","Referrer","Host","Access-Control-Allow-Origin","sentry-trace"},
