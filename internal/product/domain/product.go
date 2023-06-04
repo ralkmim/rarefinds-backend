e40c2fdde8f061e76014588c8d977909abc7c70a
Rafael Alkmim <rafael.alkmim@losch.lu>
2023-06-04 08:30:57 +0100

    validate product

--- internal/product/domain/product.go
+++ internal/product/domain/product.go
@@ -4,15 +4,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
 	ID 					primitive.ObjectID		`json:"_id" bson:"_id"`
-	Name				string					`json:"name" bson:"name"`
+	Name				string					`json:"name" bson:"name" validate:"required"`
 	Description			string					`json:"description" bson:"description"`
-	Price 				float64					`json:"price" bson:"price"`
+	Price 				float64					`json:"price" bson:"price" validate:"required"`
 	Image				string					`json:"image" bson:"image"`
 	Certified			bool					`json:"certified" bson:"certified"`
-	Type 				string					`json:"type" bson:"type"`		
-	Condition			string					`json:"condition" bson:"condition"`
+	Type 				string					`json:"type" bson:"type" validate:"required"`		
+	Condition			string					`json:"condition" bson:"condition" validate:"required"`
 	From				string					`json:"from" bson:"from"`
-	LimitedEdition		bool					`json:"limited_edition" bson:"limited_edition"`
+	LimitedEdition		bool					`json:"limited_edition" bson:"limited_edition" validate:"required"`
 	OwnerID				string					`json:"owner_id" bson:"owner_id"`
 	CreatedAt			string					`json:"created_at" bson:"created_at"`
 	UpdatedAt			string					`json:"updated_at" bson:"updated_at"`
